9ee9c4e58623a7b7b3d9085f4110f15022c1e568
kimkangtae <tt997768@gmail.com>
2023-12-22 01:13:22 +0900

    하단 네비게이션 바 구성

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:youtube_clone/youtube_main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,29 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      darkTheme: ThemeData.dark(),
+      home: const YoutubeAppClone(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
---
+++ lib/youtube_main_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class YoutubeAppClone extends StatefulWidget {
+  const YoutubeAppClone({super.key});
+
+  @override
+  State<YoutubeAppClone> createState() => _YoutubeAppCloneState();
+}
+
+class _YoutubeAppCloneState extends State<YoutubeAppClone> {
+  var _index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: SvgPicture.asset(
+          'assets/youtube_icon/youtube_premium_logo.svg',
+          width: 150,
+        ),
+      ),
+      body: const Center(
+        child: Text('메인페이지'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: (index) {
+          setState(() {
+            _index = index;
+          });
+        },
+        currentIndex: _index,
+        items: <BottomNavigationBarItem>[
+          const BottomNavigationBarItem(
+            label: '홈',
+            icon: Icon(
+              Icons.home_filled,
+              color: Colors.grey,
+            ),
+          ),
+          BottomNavigationBarItem(
+
+            icon: SvgPicture.asset(
+              'assets/youtube_icon/youtube_shorts.svg',
+              height: 25,
+            ),
+            label: 'Shorts',
+          ),
+          const BottomNavigationBarItem(
+            label: '',
+            icon: Icon(
+              Icons.add_circle_outline,
+              color: Colors.grey,
+              size: 40,
+            ),
+          ),
+          const BottomNavigationBarItem(
+            label: '구독',
+            icon: Icon(
+              Icons.subscriptions_outlined,
+              color: Colors.grey,
+            ),
+          ),
+          const BottomNavigationBarItem(
+            label: '나',
+            icon: Icon(
+              Icons.person,
+              color: Colors.grey,
+            ),
+          ),
+        ],
+
+      ),
+    );
+  }
+}
